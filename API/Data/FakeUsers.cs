a325a131e71a17733985a865220dfa3485b6d5c2
kanako001 <kunleanako@gmail.com>
2024-03-12 13:22:31 -0400

    Walking skeleton API

---
+++ API/Data/FakeUsers.cs
@@ -0,0 +1,20 @@
+﻿using API.Entities;
+using Bogus;
+
+namespace API.Data
+{
+    public class FakeUsers
+    {
+        public List<AppUser> GetAppUsers()
+        {
+            var userId = 1;
+            var userName = string.Empty;
+            var userFaker = new Faker<AppUser>()
+            .RuleFor(u => u.Id, _ => userId++)
+            .RuleFor(u => u.UserName, _ => userName);
+
+            var users = userFaker.Generate(1000);
+            return users;
+        }
+    }
+}
